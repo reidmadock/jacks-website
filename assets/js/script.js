8d60b0ee88276376ec89dba896ad8bf1f199f698
Reid M <reidmadock@gmail.com>
2024-10-08 08:33:44 -0500

    playing with tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+// Probably need to adjust this function, but not sure
+// W3 provided the base code and I assume they know what they're doing
 function openCity(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
+        // tabcontent[i].className.replace(" active","not-active");
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
